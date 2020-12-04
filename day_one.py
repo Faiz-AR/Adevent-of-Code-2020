ba16eb41fda7cb6e43aa4f8bc9fcdab361290aa8
Faiz-AR <74257945+Faiz-AR@users.noreply.github.com>
2020-12-04 20:41:41 +0800

    Updated for second part

--- day_one.py
+++ day_one.py
@@ -1,20 +1,20 @@
 # List can be found here: https://adventofcode.com/2020/day/1/input
 
 start = 0
-loop = True
 
-while loop:
+while True:
     for i in lst:
         x = i
         for j in lst[start+1:]:
             y = j
-            if x + y == 2020:
-                first_number = x
-                second_number = y
-                print(f'First number: {first_number}\nSecond Number: {second_number}')
-                multiply = first_number * second_number
-                print(f'Multiply both, we get: {multiply}')
-                loop = False
-            else:
-                continue
+            for k in lst[start+2:]:
+                z = k
+                if x + y + z == 2020:
+                    first_number = x
+                    second_number = y
+                    third_number = z
+                    print(f'First number: {first_number}\nSecond Number: {second_number}\nThird Number: {third_number}')
+                    multiply = first_number * second_number * third_number
+                    print(f'Multiply all, we get: {multiply}')
+                    exit()
         start +=1
